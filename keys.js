140424b602ff4f3c8cb2ff790614a22c0e0ba893
Brian Mejia <Bmejia319@gmail.com>
2018-08-20 00:02:19 -0400

    set up files

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log("This is loaded");
+
+exports.twitter = {
+  consumer_key: process.env.akCFK5CEBvvhkNZNfHd5w810z,
+  consumer_secret: process.env.ZZ6z2g4PSHKcnFpdvg9BT8R0GxvWau3VTNGJOCMCJM4MgQzogf,
+  access_token_key: process.env.ZZ6z2g4PSHKcnFpdvg9BT8R0GxvWau3VTNGJOCMCJM4MgQzogf,
+  access_token_secret: process.env.Mld3NNgMHAM39S2GQcyG6r711p4wOnCHGplGNKiu3N3qT
+};
+
+exports.spotify = {
+  id: process.env.f07dcaf4486b4c40a3965381fe881aa7,
+  secret: process.env.a7a020f0419d44bfb2444e0e5c522edc
+};
